cabfc442d60767ba8f2df49a9321f90d093d05d8
Артуриус <@DESKTOP>
2024-03-30 10:26:21 +0300

    Добавьте файлы проекта.

---
+++ lesson74/lesson74.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int m;
+    int k;
+    cout << "Input n m and k";
+    cin >> n >> m >> k;
+
+    if (k < n * m && ((k % n == 0) || (k % m == 0))) {
+       return true;
+ 
+    }
+    else {
+        return false;
+    }
+
+}
+
+ 
